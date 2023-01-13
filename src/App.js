be885d12db3367e3bf732c0fb87a20269212acff
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-01-13 17:26:01 +0530

    different color loader added

--- src/App.js
+++ src/App.js
@@ -6,25 +6,47 @@ import News from './Components/News';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,  
+  Route,
 } from "react-router-dom";
+
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
   // c = "John"
+  state = {
+    progress: 0,
+    loadcolor: 'white'
+  }
+
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
+
+  setLoaderColor = (loadcolor) => {
+    this.setState({ loadcolor: loadcolor })
+  }
+
   render() {
     return (
       <Router>
-       {/* <div>App {this.c}</div> */}
+        {/* <div>App {this.c}</div> */}
         <div>
           <Navbar />
+          <LoadingBar
+            height={3}
+            color={this.state.loadcolor}
+            progress={this.state.progress}
+          // onLoaderFinished={() => setProgress(0)}
+          />
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={5} country='in' category="general" scolor="primary"/>} />
-            <Route exact path="/business" element={<News key="business" pageSize={5} country='in' category="business" scolor="success" />} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={5} country='in' category="sports" scolor="secondary" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country='in' category="entertainment" scolor="danger" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={5} country='in' category="health" scolor="dark" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={5} country='in' category="science" scolor="warning" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={5} country='in' category="technology" scolor="info" />} />
+            <Route exact path="/" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#0d6efd" key="general" pageSize={5} country='in' category="general" scolor="primary" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#198754" key="business" pageSize={5} country='in' category="business" scolor="success" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#6c757d" key="sports" pageSize={5} country='in' category="sports" scolor="secondary" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#dc3545" key="entertainment" pageSize={5} country='in' category="entertainment" scolor="danger" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#212529" key="health" pageSize={5} country='in' category="health" scolor="dark" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#ffc107" key="science" pageSize={5} country='in' category="science" scolor="warning" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} setLoaderColor={this.setLoaderColor} loadcolor="#0dcaf0" key="technology" pageSize={5} country='in' category="technology" scolor="info" />} />
           </Routes>
         </div>
       </Router>
